d4d7469c5a63047b174359401549f9628837c409
Kai Hendry <hendry@webconverger.com>
2013-04-01 13:35:43 +0800

    Introduce the markdown preview thing.

--- edit/index.php
+++ edit/index.php
@@ -7,6 +7,11 @@ body {
 }
 textarea { width: 100%; height: 20em; }
 </style>
+<link rel="stylesheet" href="/pagedown/demo.css">
+<script src="/pagedown/Markdown.Converter.js"></script>
+<script src="/pagedown/Markdown.Sanitizer.js"></script>
+<script src="/pagedown/Markdown.Editor.js"></script>
+<script src="/pagedown/jquery-1.9.1.min.js"></script>
 <title>Page editor</title>
 </head>
 <body>
@@ -29,9 +34,14 @@ if (isset($_POST['style'])) {
 
 <form method=post>
 <h1>Content</h1>
-<textarea name=content>
+<div class="wmd-panel">
+<div id="wmd-button-bar"></div>
+<textarea class="wmd-input" id="wmd-input" name="content">
 <?php readfile($content); ?>
 </textarea>
+</div>
+<div id="wmd-preview" class="wmd-panel wmd-preview"></div>
+
 
 <p>Write in HTML or <a href="https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet">markdown</a>.</p>
 
@@ -42,6 +52,12 @@ if (isset($_POST['style'])) {
 </textarea>
 
 <input type=submit>
+
+<script>
+     var converter1 = Markdown.getSanitizingConverter();
+            var editor1 = new Markdown.Editor(converter1);
+            editor1.run();
+</script>
 </form>
 </body>
 </html>
