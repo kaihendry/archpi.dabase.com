a6a11b6925f72ef7ba6ae76daad7518d67a19971
Kai Hendry <hendry@webconverger.com>
2013-06-21 12:23:12 +0800

    Converted to http://markdown.pioul.fr/

--- edit/index.php
+++ edit/index.php
@@ -28,11 +28,10 @@ if (getAdmin() && isset($_POST['content'])) {
 body { font-family: "Helvetica Neue", sans-serif; }
 textarea { width: 100%; height: 20em; }
 </style>
-<link rel="stylesheet" href="/pagedown/demo.css">
-<script src="/pagedown/Markdown.Converter.js"></script>
-<script src="/pagedown/Markdown.Sanitizer.js"></script>
-<script src="/pagedown/Markdown.Editor.js"></script>
-<script src="/pagedown/jquery-1.9.1.min.js"></script>
+<link rel="stylesheet" href="/edit/main.css">
+<script src="/edit/jquery.js"></script>
+<script src="/edit/main.js"></script>
+<script src="/edit/showdown.js"></script>
 <title>Editing <?php echo $content; ?></title>
 </head>
 <body>
@@ -48,14 +47,102 @@ if (is_dir_empty($cc)) {
 	echo '<p><input type=submit value=Save>Found cookie!</p>';
 } else { die('<p>You dont have write permissions</p>'); }
 ?>
+		<div id="left_column">
+			<div id="top_panels_container">
+				<div class="top_panel" id="quick_reference">
+					<div class="close">×</div>
+					<table>
+						<tr>
+							<td>
+								<pre><code><span class="highlight">*</span>This is italicized<span class="highlight">*</span>, and <span class="highlight">**</span>this is bold<span class="highlight">**</span>.</code></pre>
+							</td>
+							<td><p>Use <code>*</code> or <code>_</code> for emphasis.</p></td>
+						</tr>
+						<tr>
+							<td>
+								<pre><code>This is a first level header
+<?php							?><span class="highlight">============================</span></code></pre>
+							</td>
+							<td><p>You can alternatively put hash marks at the beginning of the line: <code>#&nbsp;H1&nbsp;#</code>, <code>##&nbsp;H2&nbsp;##</code>, <code>###&nbsp;H3&nbsp;###</code>...</p></td>
+						</tr>
+						<tr>
+							<td>
+								<pre><code>This is a link to <span class="highlight">[Google](http://www.google.com)</span></code></pre>
+							</td>
+							<td><p></p></td>
+						</tr>
+						<tr>
+							<td>
+								<pre><code>First line.<span class="highlight">  </span>
+<?php							?>Second line.</code></pre>
+							</td>
+							<td><p>End a line with two spaces for a linebreak.</p></td>
+						</tr>
+						<tr>
+							<td>
+								<pre><code><span class="highlight">- </span>Unordered list item
+<?php							?><span class="highlight">- </span>Unordered list item</code></pre>
+							</td>
+							<td><p>Unordered (bulleted) lists use asterisks, pluses, and hyphens (<code>*</code>, <code>+</code>, and <code>-</code>) as list markers.</p></td>
+						</tr>
+						<tr>
+							<td>
+								<pre><code><span class="highlight">1. </span>Ordered list item
+<?php							?><span class="highlight">2. </span>Ordered list item</code></pre>
+							</td>
+							<td><p>Ordered (numbered) lists use regular numbers, followed by periods, as list markers.</p></td>
+						</tr>
+						<tr>
+							<td><pre><code><span class="highlight">    </span>/* This is a code block */</code></pre></td>
+							<td><p>Indent four spaces for a preformatted block.</p></td>
+						</tr>
+						<tr>
+							<td><pre><code>Let's talk about <span class="highlight">`</span>&lt;html&gt;<span class="highlight">`</span>!</code></pre></td>
+							<td><p>Use backticks for inline code.</p></td>
+						</tr>
+						<tr>
+							<td>
+								<pre><code><span class="highlight">![Valid XHTML](http://w3.org/Icons/valid-xhtml10)</span></code></pre>
+							</td>
+							<td><p>Images are exactly like links, but they have an exclamation point in front of them.</p></td>
+						</tr>
+					</table>
+				</div>
+				<div class="top_panel" id="about">
+					<div class="close">×</div>
+					<p>Hi, I'm <a href="http://pioul.fr/a-propos/" target="_blank">Philippe Masset</a>.</p>
+					<p>I made this minimalist online Markdown editor in a couple of hours just for the heck of it.<br/>And also maybe because I love Markdown (and front end stuff).</p>
+					<p>If you have any suggestions or remarks whatsoever, just click on my name above and you'll have plenty of ways of contacting me.</p>
+					<p>Credit is due to <a href="http://daringfireball.net/" target="_blank">John Gruber</a> for creating <a href="http://daringfireball.net/projects/markdown/" target="_blank">Markdown</a> in the first place, and John Fraser for his amazing JavaScript port, <code>showdown.js</code>.</p>
+				</div>
+			</div>
 
-<div class="wmd-panel">
-<div id="wmd-button-bar"></div>
-<textarea class="wmd-input" id="wmd-input" name="content">
+<div class="wrapper">
+<div class="overlay">
+<a href="#" class="button toppanel" data-toppanel="quick_reference">Quick Reference</a>
+<a href="http://daringfireball.net/projects/markdown/syntax" target="_blank" class="button">Markdown</a>
+<a href="#" class="button toppanel" data-toppanel="about">About</a>
+<div class="clear"></div>
+</div>
+
+<textarea id="markdown" name="content" class="full-height">
 <?php readfile($content); ?>
 </textarea>
 </div>
-<div id="wmd-preview" class="wmd-panel wmd-preview"></div>
+</div>
+<div id="right_column">
+<div class="wrapper">
+<div class="overlay">
+<a href="#" class="button switch" data-switchto="html">HTML</a>
+<a href="#" class="button switch" data-switchto="preview">Preview</a>
+<div class="clear"></div>
+</div>
+<textarea id="html" class="full-height"></textarea>
+<div id="preview" class="full-height"></div>
+</div>
+</div>
+
+
 
 <textarea id="styleeditor" name=style>
 <?php readfile($style); ?>
@@ -70,12 +157,6 @@ if (is_dir_empty($cc)) {
 <input value="Upload" type="submit">
 </form>
 <?php } ?>
-
-<script>
-var converter1 = Markdown.getSanitizingConverter();
-var editor1 = new Markdown.Editor(converter1);
-editor1.run();
-</script>
 
 </body>
 </html>
